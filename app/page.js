95d4af536d9c97ecb71619e30be13239a22c1da3
amdadul-haque <amdadul1807107@gmail.com>
2023-12-04 01:39:20 +0600

    logo is big

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main>
       <div className='max-w-[969px] mx-auto'>
         <div className='flex flex-col items-center gap-9'>
-          <img src="/assets/logo-text.png" className='h-[150px] sm:h-[200px] md:h-[350px] lg:h-[450px]' alt="logo-with-text" />
+          <img src="/assets/logo-text.png" className='h-[210px] sm:h-[280px] md:h-[380px] lg:h-[450px]' alt="logo-with-text" />
           <Link href="/">
             <img src="/assets/app-store-bg.png" alt="app store" className='h-[60px] md:h-[90px]' />
           </Link>
